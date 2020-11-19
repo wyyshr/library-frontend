63f37aeee90c3e66d1e40068cf907f71c445da2f
wyyshr <2015599782@qq.com>
2020-11-19 16:03:19 +0800

    清除定时器

--- src/views/scanCode/ScanCode.tsx
+++ src/views/scanCode/ScanCode.tsx
@@ -15,17 +15,22 @@ const ScanCode: React.FC<RouteComponentProps> = (props) => {
     ajax.get(path.checkOrder) // 检查订单
     const res = await ajax.get(path.getScanCode)
     const url = res as any as string
-    message.success('二维码更新成功')
     setCodeUrl(url)
+    message.success('二维码更新成功')
   }
 
   // 每隔5分钟刷新二维码
-  setInterval(() => {
+  const timer = setInterval(() => {
     getScanCode();    // 获取二维码
   }, 300000);
 
   /** componentDidMount */
-  React.useEffect(()=>{getScanCode()},[])
+  React.useEffect(()=>{
+    getScanCode()
+    return () => {
+      clearInterval(timer); // 清除定时器
+    }
+  },[])
 
   const imgUrl = "https://www.17sucai.com/preview/1257759/2018-12-25/%E6%88%91%E7%9A%84%E4%B9%A6%E5%B1%8B/img/bgImg.jpg"
   return (
